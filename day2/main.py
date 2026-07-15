52c249bd79d44bbe768a3dfd91f1ce8a609bc54e
Himanshu Jha <himanshuprincejha2001@gmail.com>
2026-07-15 23:27:12 +0530

    feat: implement OpenAI client integration to query Ollama for model responses

--- day2/main.py
+++ day2/main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from day2!")
 
+import os
+# from pathlib import Path
+from dotenv import load_dotenv
 
-if __name__ == "__main__":
-    main()
+from openai import OpenAI
+
+load_dotenv()
+
+ollama_base_url = os.getenv("OLLAMA_BASE_URL")
+my_api_key = "ollama"
+
+client = OpenAI(
+    base_url = ollama_base_url,
+    api_key = my_api_key
+)
+
+model = os.getenv("OLLAMA_MODEL")
+
+message_system =  {
+    "role": "system",
+    "content": "You are my Senior Engineer."
+}
+
+message_user = {
+    "role": "user",
+    "content": "What is dynamic Programming"
+}
+
+Messages = [
+   message_system, message_user
+]
+
+response = client.chat.completions.create(
+    model=model,
+    messages=Messages
+)
+
+print(response.choices[0].message.content)
